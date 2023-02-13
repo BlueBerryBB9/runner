9fbc07863c48b7a0fb1faea8c6521ead016108bd
Martin Leroy <martin.leroy@ecole-89.com>
2023-02-13 12:10:33 +0100

    runner Sauvegarde

---
+++ unprep/draw_end.c
@@ -0,0 +1,14 @@
+/*
+ * E89 Pedagogical & Technical Lab
+ * project:     runner
+ * created on:  2023-01-30 - 09:05 +0100
+ * 1st author:  martin.leroy - martin.leroy
+ * description: draw end screen
+ */
+
+#include "graphic.h"
+
+void draw_end(struct map *map, struct map *nmap, t_bunny_pixelarray *px)
+{
+    
+}
