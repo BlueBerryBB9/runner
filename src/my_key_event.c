76f6659272a40011fe173fe273d791835af214b0
Martin Leroy <martin.leroy@ecole-89.com>
2023-02-28 15:31:59 +0100

    runner Sauvegarde

--- src/my_key_event.c
+++ src/my_key_event.c
@@ -73,7 +73,7 @@ t_bunny_response my_key_event(t_bunny_event_state state,
     refresh_map(&ds->map, ds->px);
     first_person(ds, 69);
     refresh(ds->win_fp, ds->px_fp);
-    //draw_pacman(ds->px, ds->pos, ds->direction, ds->map.tile_size / 5);
+    draw_pacman(ds->px, ds->pos, ds->direction, ds->map.tile_size / 5);
     bpos = pos_from_accurate(&ds->pos);
     put_pixel(ds->px, &bpos, GREEN);
     refresh(ds->win, ds->px);
